7a833b8b80b875c7c5baf943a64b304f5ef8e579
Alex baulderstone <alex.baulderstone@coderacademy.edu.au>
2021-02-02 13:44:55 +1030

    sets up validation on dob date format

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,6 @@
 class Author < ApplicationRecord
     has_one :address
+    validate :validates_dob_format
 
     def full_name
         "#{self.first_name} #{self.last_name}"
@@ -8,4 +9,14 @@ class Author < ApplicationRecord
     def age
         ((Time.now - self.date_of_birth.to_time) / 1.year.seconds).floor
     end 
+
+    private 
+
+    def validates_dob_format
+        begin
+            self.date_of_birth.to_date
+        rescue 
+            errors.add(:date_of_birth, "Invalid birthdate")
+        end 
+    end
 end
